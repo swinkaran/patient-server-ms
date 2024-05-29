3b5153b63690b8bda5e6c6bf737cb1938f710fc5
Swinkaran <swinkaran@hotmail.com>
2024-05-29 17:23:23 +1000

    Added function 2
    Added function 2

--- src/patient-server-service-api/Patient.Server.Service.API/Patient.Server.Service.Funcz/Function1.cs
+++ src/patient-server-service-api/Patient.Server.Service.API/Patient.Server.Service.Funcz/Function1.cs
@@ -2,12 +2,19 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Xml.Linq;
 
 namespace Patient.Server.Service.Funcz
 {
     public class Function1
     {
         private readonly ILogger<Function1> _logger;
+        public class MyData
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
 
         public Function1(ILogger<Function1> logger)
         {
@@ -19,6 +26,26 @@ namespace Patient.Server.Service.Funcz
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
             return new OkObjectResult("Welcome to Azure Functions!");
+        }
+
+        [Function("UpdateStock")]
+        public static async Task<IActionResult> RunFunctionPost(
+        [HttpTrigger(AuthorizationLevel.Function, "post", Route = "stock/")] HttpRequest req,
+        ILogger log)
+        {
+            //log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            MyData data = JsonConvert.DeserializeObject<MyData>(requestBody);
+
+            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            //dynamic data = JsonConvert.DeserializeObject(requestBody);
+            //name = name ?? data?.name;
+
+            string name = data == null ? string.Empty : data.Name;
+            return name != null
+                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
 }
