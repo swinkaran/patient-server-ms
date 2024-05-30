3ba8a7250a3b534c546f270f692005c8f1dde771
Swinkaran <swinkaran@hotmail.com>
2024-05-30 11:54:49 +1000

    added models

---
+++ src/patient-server-service-api/Patient.Server.Service.API/Patient.Server.Service.Funcz/PatientAddressViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Patient.Server.Service.Funcz
+{
+    public class PatientAddressViewModel
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+        public string Suburb { get; set; }
+        public string PostCode { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ src/patient-server-service-api/Patient.Server.Service.API/Patient.Server.Service.Funcz/PatientRequest.cs
@@ -0,0 +1,23 @@
+﻿namespace Patient.Server.Service.Funcz
+{
+    public class PatientRequest
+    {
+        public string? PatientProfileId { get; set; }
+        public string AttendeeId { get; set; }
+        public string BookingId { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Gender { get; set; }
+        public string Dob { get; set; }
+        public string Address { get; set; }
+        public string Suburb { get; set; }
+        public string PostCode { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string AttendeeEmail { get; set; }
+        public string PhoneType { get; set; }
+        public string Phone { get; set; }
+        public string MedicareNumber { get; set; }
+        public string IRN { get; set; }
+    }
+}
